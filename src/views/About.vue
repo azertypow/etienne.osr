184504f989a6c9900201cd97ae9d9a854d45dba6
azertypow <azertypow@gmail.com>
2021-05-14 21:10:04 +0200

    init

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="v-about">
+    <router-link to="/" class="home__next"></router-link>
+    <img class="e-img-bg" alt="" src="../assets/plan/02_Who_are_you@2x.png">
+  </section>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from "vue"
+
+export default defineComponent({
+
+  name: 'About',
+
+});
+</script>
+
+<style scoped lang="scss">
+.home__next {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  display: block;
+  background: blue;
+  width: 200px;
+  height: 200px;
+  z-index: 100;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <router-link to="/about" class="home__next"></router-link>
+    <img class="e-img-bg" alt="" src="../assets/plan/01_INTERFACE.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home__next {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  display: block;
+  //background: blue;
+  width: 200px;
+  height: 200px;
+  z-index: 100;
+  transform: translate(-50%, -50%);
+}
+</style>
